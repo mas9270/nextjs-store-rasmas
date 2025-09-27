d52c7fda72c776e1c20cfe769d31667f8157f17a
@mas9270 <mas.ahm92@gmail.com>
2025-09-27 14:12:50 +0330

    اضافه کردن error.tsx

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { Box, Button } from '@mui/material'
+import React from 'react'
+
+export default function ErrorPage({ error, refresh }: { error: string, refresh: () => void }) {
+    return (
+        <Box display={"flex"} justifyContent={"center"} alignItems={"center"} flexDirection={"row"}>
+            <Box p={1}>{error}</Box>
+            <Button onClick={refresh} style={{ margin: "5px" }} >تلاش دوباره</Button>
+        </Box>
+    )
+}
